b880ff4c0a2ca8a1f08fd1585e6586579b98709e
ASHISH PANIGRAHI <166148066+Ashishpanigrahi9777@users.noreply.github.com>
2026-07-09 20:40:44 +0530

    Prepare Django app for Render deployment

--- school_management/settings.py
+++ school_management/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    
+]
 
 ROOT_URLCONF = "school_management.urls"
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
